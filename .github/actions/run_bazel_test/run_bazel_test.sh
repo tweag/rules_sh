896dd95037b441f87fc100b44297d16b4044c857
Claudio Bley <claudio.bley@tweag.io>
2024-12-02 13:51:38 +0100

    Try to bisect between good..unavailable

--- .github/actions/run_bazel_test/run_bazel_test.sh
+++ .github/actions/run_bazel_test/run_bazel_test.sh
@@ -24,4 +24,8 @@ fi
 
 #USE_BAZEL_VERSION=7.2.1 bazelisk test  --build_tests_only "${bzl_pkgs}"
 
-bazelisk --bisect=7.2.1..7.3.0 test  --build_tests_only "${bzl_pkgs}"
+# good commits:
+# - 000a83a77b417823e978f4b905a8039b2dd38ef3
+# - 6d69b9c6003a963bf9e26b3d52b3a736e900cee0
+# failed (no binary): 282ac623df3523e2e31a2de9c002e5c50da19fec
+bazelisk --bisect=6d69b9c6003a963bf9e26b3d52b3a736e900cee0..282ac623df3523e2e31a2de9c002e5c50da19fec test  --build_tests_only "${bzl_pkgs}"
