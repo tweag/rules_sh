78dc5830513764521fc89540b075b01f54123d75
Claudio Bley <claudio.bley@tweag.io>
2024-12-05 12:51:19 +0100

    Call bash wrapper on Windows
    - remove powershell wrapper script (it was not equivalent to the bash script)

--- tools/bazel.bat
+++ tools/bazel.bat
@@ -1,1 +1,12 @@
-@pwsh -File "%~dp0bazel_wrapper.ps1" %*
+@echo off
+
+:: Check if bash is available
+where bash >nul 2>nul
+if errorlevel 1 (
+   echo Bash is not installed or not in PATH. Please install WSL or add bash to PATH.
+   exit /b 1
+)
+
+:: Call the Bash wrapper
+bash --noprofile --norc -o errexit -o nounset -o pipefail "%~dp0bazel" %*
+
